c768db461253d263b675242ae9541e20144a5f5a
David Salomón Rodríguez Ardila <rodriguez.ardila15@gmail.com>
2023-05-02 17:27:15 +0200

    added component inicio

--- frontend-aws-cruz-roja/src/app/app.module.ts
+++ frontend-aws-cruz-roja/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
